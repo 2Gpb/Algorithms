4878415659ff79e69bb30aeed4e0a3ea594c27eb
2Gpb <Pvtyukhmenev@gmail.com>
2024-12-29 13:42:58 +0300

    change selection sort

--- SelectionSort/SelectionSort.playground/Contents.swift
+++ SelectionSort/SelectionSort.playground/Contents.swift
@@ -1,17 +1,15 @@
-final class SelectionSort {
-    static func sort(_ array: inout [Int]) -> [Int] {
-        for i in 0..<array.count-1 {
-            var smallest = i
-            for j in i+1..<array.count {
-                if array[j] < array[smallest] {
-                    smallest = j
-                }
+func selectionSort<T: Comparable>(_ array: inout [T]) -> [T] {
+    for i in 0..<array.count - 1 {
+        var smallest = i
+        for j in i + 1..<array.count {
+            if array[j] < array[smallest] {
+                smallest = j
             }
-            array.swapAt(i, smallest)
         }
-        return array
+        array.swapAt(i, smallest)
     }
+    return array
 }
 
 var array = [10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26]
-print(SelectionSort.sort(&array))
+print(selectionSort(&array))
