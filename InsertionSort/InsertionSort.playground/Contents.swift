8b91a7672f02a389890d963adbe77fddc6984055
2Gpb <Pvtyukhmenev@gmail.com>
2024-12-29 13:37:03 +0300

    add insertion sort

--- InsertionSort/InsertionSort.playground/Contents.swift
+++ InsertionSort/InsertionSort.playground/Contents.swift
@@ -1,6 +1,15 @@
-import UIKit
+func insertionSort<T: Comparable>(_ array: inout [T]) -> [T] {
+    for i in 1..<array.count {
+        let key = array[i]
+        var j = i - 1
+        while j >= 0 && array[j] > key {
+            array[j + 1] = array[j]
+            j -= 1
+        }
+        array[j + 1] = key
+    }
+    return array
+}
 
-var greeting = "Hello, playground"
-
-print("aaa")
-
+var array = [10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26]
+print(insertionSort(&array))
