4a2f6ee9956529108273f3a4b598402f2f79a749
2Gpb <Pvtyukhmenev@gmail.com>
2024-12-29 16:45:56 +0300

    add first variant bucket sort

---
+++ BucketSort/BucketSort-firstVariant/BucketSort.playground/Contents.swift
@@ -0,0 +1,28 @@
+func bucketSort(_ array: inout [Int]) -> [Int] {
+    var maxValue = array[0]
+    var minValue = array[0]
+    
+    for value in array {
+        if value > maxValue {
+            maxValue = value
+        }
+        if value < minValue {
+            minValue = value
+        }
+    }
+    
+    let bucketCount = maxValue - minValue + 1
+    var buckets: [[Int]] = Array(repeating: [], count: bucketCount)
+    
+    for i in array {
+        let bucketIndex = i - minValue
+        buckets[bucketIndex].append(i)
+    }
+    
+    array = buckets.flatMap { $0 }
+    
+    return array
+}
+
+var array = [10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26]
+print(bucketSort(&array))
